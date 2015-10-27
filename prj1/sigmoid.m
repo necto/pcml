56e39fb92721d2293e145a991b67dae0376e25e6
Johan Droz <johan.droz@epfl.ch>
2015-10-27 23:46:08 +0100

    Remove useless initialization

--- prj1/sigmoid.m
+++ prj1/sigmoid.m
@@ -1,7 +1,6 @@
 % Return the sigmoid of x given by the following equation:
 % f(x) = 1 / (1 + e^(-x))
 function [ sig ] = sigmoid( x )
-    sig = zeros(size(x));
     sig = 1 ./ (1 + exp(-x));
 end
 
