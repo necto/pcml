ed92426f70d515a4216224ecb225b158fcafd9b0
Johan Droz <johan.droz@epfl.ch>
2015-11-01 23:59:01 +0100

    Refactor regression

--- prj1/regression.m
+++ prj1/regression.m
@@ -1,21 +1,65 @@
-clear all;
-close all;
+if (exist('reportMode', 'var') == 1)
+    forReport = true;
+else
+    clear all;
+    close all;
+    forReport = false;
+    %Possible values: 'leastSqGD', 'leastSq', 'removal', 'dummy',
+    %'ridgeReg';
+    stage = 'ridgeReg';
+end;
+
 load('data/regression.mat');
 
-%% Remove outliers
-outliers = getOutliers(X_train);
-X_train = X_train(outliers==0,:);
-y_train = y_train(outliers==0);
+%% Linear regression using gradient descent
+if (strcmp(stage, 'leastSqGD'))
+  disp('Linear regression using gradient descent');
+  seeds = [1:10 42 43 7500 100500];
+  errorTe = zeros(length(seeds), 1);
+  errorTr = zeros(length(seeds), 1);
+  for s = 1:length(seeds)
+    seed = seeds(s);
+    [XTr, yTr, XTe, yTe] = split(y_train, X_train, 0.9, seed);
+    [XTr, XTr_mean, XTr_std] = normalize(XTr);
+    XTe = adjust(XTe, XTr_mean, XTr_std);
+    tXTr = [ones(size(XTr, 1), 1) XTr];
+    tXTe = [ones(size(XTe, 1), 1) XTe];
 
-%% Feature removal
-c = corr(X_train,y_train);
-idx = find(abs(c)>0.5);
-X_train = X_train(:,idx);
-figure;
-plot(corr(X_train,y_train));
+    lSqGdBeta = leastSquaresGD(yTr, tXTr, 0.6);
 
-%% Enable dummy coding for X_train columns [2,12,14,29,48,62]
-% X_train = dummyCoding(X_train, [2,12,14,29,48,61]);
+    errorTe(s) = computeCost( yTe, tXTe, lSqGdBeta );
+    errorTr(s) = computeCost( yTr, tXTr, lSqGdBeta );
+  end;
+
+  lSqGdTestRMSE = sqrt(2*mean(errorTe));
+  lSqGdTrainRMSE = sqrt(2*mean(errorTr));
+  fprintf('Test RMSE:  %d\nTrain RMSE: %d\n',lSqGdTestRMSE,lSqGdTrainRMSE);
+end;
+
+%% Least squares using normal equations
+if (strcmp(stage, 'leastSq'))
+  disp('Least squares using normal equations');
+  seeds = [1:10 42 43 7500 100500];
+  errorTe = zeros(length(seeds), 1);
+  errorTr = zeros(length(seeds), 1);
+  for s = 1:length(seeds)
+    seed = seeds(s);
+    [XTr, yTr, XTe, yTe] = split(y_train, X_train, 0.9, seed);
+    [XTr, XTr_mean, XTr_std] = normalize(XTr);
+    XTe = adjust(XTe, XTr_mean, XTr_std);
+    tXTr = [ones(size(XTr, 1), 1) XTr];
+    tXTe = [ones(size(XTe, 1), 1) XTe];
+
+    lSqBeta = leastSquares(yTr, tXTr);
+
+    errorTe(s) = computeCost( yTe, tXTe, lSqBeta );
+    errorTr(s) = computeCost( yTr, tXTr, lSqBeta );
+  end;
+
+  lSqTestRMSE = sqrt(2*mean(errorTe));
+  lSqTrainRMSE = sqrt(2*mean(errorTr));
+  fprintf('Test RMSE:  %d\nTrain RMSE: %d\n',lSqTestRMSE,lSqTrainRMSE);
+end;
 
 %% Split data into train and validation sets
 [XTr, yTr, XTe, yTe] = split(y_train, X_train, 0.9, 42);
@@ -27,7 +71,7 @@ tXTr = [ones(size(XTr, 1), 1) XTr];
 tXTe = [ones(size(XTe, 1), 1) XTe];
 
 % Generate indexes for K-fold cross-validation
-K = 4;
+K = 7;
 N = size(yTr, 1);
 idx = randperm(N);
 Nk = floor(N/K);
@@ -36,125 +80,58 @@ for k = 1:K
     idxCV(k,:) = idx(1 + (k-1)*Nk:k*Nk);
 end;
 
-%% Linear regression using gradient descent
-disp('Linear regression using gradient descent');
-errorTeSub = zeros(K, 1);
-errorTrSub = zeros(K, 1);
-for k = 1:K
-    [yTrTe, yTrTr, tXTrTe, tXTrTr] = split4crossValidation(k, idxCV, yTr, tXTr);
-    beta = leastSquaresGD(yTrTr, tXTrTr, 0.6);
-    errorTeSub(k) = computeCost(yTrTe,tXTrTe,beta); 
-    errorTrSub(k) = computeCost(yTrTr,tXTrTr,beta); 
-end
-rmseTr = sqrt(2*mean(errorTrSub))
-rmseTe = sqrt(2*mean(errorTeSub))
-
-%% Least squares using normal equations
-disp('Least squares using normal equations');
-errorTeSub = zeros(K, 1);
-errorTrSub = zeros(K, 1);
-for k = 1:K
-    [yTrTe, yTrTr, tXTrTe, tXTrTr] = split4crossValidation(k, idxCV, yTr, tXTr);
-    beta = leastSquares(yTrTr, tXTrTr);
-    errorTeSub(k) = computeCost(yTrTe,tXTrTe,beta); 
-    errorTrSub(k) = computeCost(yTrTr,tXTrTr,beta); 
-end
-rmseTr = sqrt(2*mean(errorTrSub))
-rmseTe = sqrt(2*mean(errorTeSub))
-%{
-%% Separate data into three different models
-% Seperate data into 3 clouds
-id1 = 1;
-id2 = 2;
-
-tXTr1 = tXTr(yTr<=5500,:);
-yTr1 = yTr(yTr<=5500,:);
-tXTr2 = tXTr(yTr>5500 & yTr<=10000 & tXTr(:,id2)>0.25 & tXTr(:,id2)<6.4,:);
-yTr2 = yTr(yTr>5500 & yTr<=10000 & tXTr(:,id2)>0.25 & tXTr(:,id2)<6.4,:);
-tXTr3 = tXTr(yTr>=10000,:);
-yTr3 = yTr(yTr>=10000,:);
-figure;
-plot(tXTr1(:,id2),yTr1,'o',tXTr2(:,id2),yTr2,'o',tXTr3(:,id2),yTr3,'o');
-title('Scatter plot of tX\_train vs y\_train');
-hx = xlabel('tX\_train (column id2)');
-hy = ylabel('y\_train');
-set(gca,'fontsize',13,'fontname','Helvetica','box','off','tickdir','out','ticklength',[.02 .02],'xcolor',0.5*[1 1 1],'ycolor',0.5*[1 1 1]);
-set([hx; hy],'fontsize',13,'fontname','avantgarde','color',[.3 .3 .3]);
-print -dpdf cluster.pdf
-figure;
-plot(tXTr1(:,id1),yTr1,'o',tXTr2(:,id1),yTr2,'o',tXTr3(:,id1),yTr3,'o');
-
-%% Least squares using normal equations
-disp('Least squares using normal equations 3 models');
-errorTeSub = zeros(K, 1);
-errorTrSub = zeros(K, 1);
-errorTeXSelect = zeros(K, 1);
-th1 = 0:0.1:1;
-th2 = 1:0.1:2;
-for i = 1:length(th1)
-  for j = 1:length(th2)
-    for k = 1:K
-        [yTrTe, yTrTr, tXTrTe, tXTrTr] = split4crossValidation(k, idxCV, yTr, tXTr);
-
-        % Separate into 3 models
-        idxTr1 = find(yTrTr<=5500);
-        idxTe1 = find(yTrTe<=5500);
-        idxTr2 = find(yTrTr>5500 & yTrTr<=10000 & tXTrTr(:,id2)>0.25 & tXTrTr(:,id2)<6.4);
-        idxTe2 = find(yTrTe>5500 & yTrTe<=10000 & tXTrTe(:,id2)>0.25 & tXTrTe(:,id2)<6.4);
-        idxTr3 = find(yTrTr>=10000);
-        idxTe3 = find(yTrTe>=10000);
-        % Display
-%         figure;
-        plot(tXTrTr(idxTr1,id2),yTrTr(idxTr1,:),'o',tXTrTr(idxTr2,id2),yTrTr(idxTr2,:),'o',tXTrTr(idxTr3,id2),yTrTr(idxTr3,:),'o');
-        figure;
-        plot(tXTrTe(idxTe1,id2),yTrTe(idxTe1,:),'o',tXTrTe(idxTe2,id2),yTrTe(idxTe2,:),'o',tXTrTe(idxTe3,id2),yTrTe(idxTe3,:),'o');
-
-        % Compute one beta per model
-        beta1 = leastSquares(yTrTr(idxTr1,:), tXTrTr(idxTr1,:));
-        beta2 = leastSquares(yTrTr(idxTr2,:), tXTrTr(idxTr2,:));
-        beta3 = leastSquares(yTrTr(idxTr3,:), tXTrTr(idxTr3,:));
-
-        errorTrSub(k) = computeCost3Clouds(yTrTr, tXTrTr, idxTr1, idxTr2, idxTr3, beta1, beta2, beta3);
-        errorTeSub(k) = computeCost3Clouds(yTrTe, tXTrTe, idxTe1, idxTe2, idxTe3, beta1, beta2, beta3);
-
-        ypred= doPrediction(tXTrTe, beta1, beta2, beta3, th1(i), th2(j), id1, id2);
-        e = yTrTe - ypred;
-        errorTeXSelect(k) = e'*e/(2*length(yTrTe));
-    end
-    err(i,j) = sqrt(2*mean(errorTeXSelect));
-  end
-end
-rmseTr = sqrt(2*mean(errorTrSub))
-rmseTe = sqrt(2*mean(errorTeSub))
-rmseT = min(min(err))
-%}
 %% Ridge regression using normal equations
-disp('Ridge regression using normal equations');
-degree = 3;
-lvals = logspace(1,5,100);
-pXTr = [ones(size(XTr, 1), 1) myPoly(XTr, degree)];
-for l = 1:length(lvals)
-    lambda = lvals(l);
-    %if mod(l,10)==0;
-    disp('tic');
-    %end
-    errorTeSub = zeros(K,1);
-    errorTrSub = zeros(K,1);
-    for k = 1:K
-        [yTrTe, yTrTr, ptXTrTe, ptXTrTr] = split4crossValidation(k, idxCV, yTr, pXTr);
+if (strcmp(stage, 'ridgeReg'))
+  disp('Ridge regression using normal equations');
+  mvals = [2];
+  lvals = logspace(1,5,10);
+  
+  errorTe = zeros(K, 1);
+  errorTr = zeros(K, 1);
+  errorTT = zeros(K, 1);
+  rmseTe = zeros(size(mvals), size(lvals));
+  rmseTr = zeros(size(mvals), size(lvals));
+  rmseTT = zeros(size(mvals), size(lvals));
+  for j = 1:length(mvals)
+    m = mvals(j);
+    pXTr = myPoly(tXTr, m);
+    pXTe = myPoly(tXTe, m);
+    for l = 1:length(lvals)
+      lambda = lvals(l);
+      for k = 1:K
+        [yTrTe, yTrTr, pXTrTe, pXTrTr] = split4crossValidation(k, idxCV, yTr, pXTr);
         
-        beta = ridgeRegression(yTrTr, ptXTrTr, lambda);
-        %yyy = ptXTrTe*beta;
-        errorTeSub(k) = computeCost(yTrTe,ptXTrTe,beta);
-        errorTrSub(k) = computeCost(yTrTr,ptXTrTr,beta);
+        beta = ridgeRegression(yTrTr, pXTrTr, lambda);
+        
+        errorTe(k) = computeCost(yTrTe,pXTrTe,beta);
+        errorTr(k) = computeCost(yTrTr,pXTrTr,beta);
+        errorTT(k) = computeCost(yTe,pXTe,beta);
+      end;
+      rmseTe(j,l) = sqrt(2*mean(errorTe)); 
+      rmseTr(j,l) = sqrt(2*mean(errorTr));
+      rmseTT(j,l) = sqrt(2*mean(errorTT));
     end;
-    errorTe(l) = mean(errorTeSub);
-    errorTr(l) = mean(errorTrSub);
+    [rmseStar lrmseStar] = min(rmseTe(1,:));
+    [rmseTrStar lrmseTrStar] = min(rmseTr(1,:));
+    [rmseTTStar lrmseTTStar] = min(rmseTT(1,:));
+    fprintf('Test  RMSE: %d\nTrain RMSE: %d\nTT    RMSE: %d\n',rmseStar,rmseTrStar,rmseTTStar);
+    figure;
+    plot(lvals,rmseTe);
+  end;
 end;
-figure;
-plot(lvals,errorTe)
 
-[errTeStar lTeStar] = min(errorTe)
-[errTrStar lTrStar] = min(errorTr)
-rmseTe = sqrt(2*errTeStar)
-rmseTr = sqrt(2*errTrStar)
+%% Remove outliers
+% outliers = getOutliers(X_train);
+% X_train = X_train(outliers==0,:);
+% y_train = y_train(outliers==0);
+
+%% Feature removal
+c = corr(X_train,y_train);
+idx = find(abs(c)>0.04);
+X_train = X_train(:,idx);
+figure;
+plot(corr(X_train,y_train));
+
+%% Enable dummy coding for X_train columns [2,12,14,29,48,62]
+% X_train = dummyCoding(X_train, [2,12,14,29,48,61]);
+
