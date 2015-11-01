cc804390ddaa9beca3cbe6c5102130445dd29f6d
Johan Droz <johan.droz@epfl.ch>
2015-11-01 22:38:34 +0100

    Commit changes before refactoring

--- prj1/regression.m
+++ prj1/regression.m
@@ -7,14 +7,18 @@ outliers = getOutliers(X_train);
 X_train = X_train(outliers==0,:);
 y_train = y_train(outliers==0);
 
-%% Enable dummy coding for X_train columns [2,12,14,29,48,62]
-X_train = dummyCoding(X_train, [2,12,14,29,48,62]);
-
 %% Feature removal
-X_train = X_train(:,[51 69]);
+c = corr(X_train,y_train);
+idx = find(abs(c)>0.5);
+X_train = X_train(:,idx);
+figure;
+plot(corr(X_train,y_train));
+
+%% Enable dummy coding for X_train columns [2,12,14,29,48,62]
+% X_train = dummyCoding(X_train, [2,12,14,29,48,61]);
 
 %% Split data into train and validation sets
-[XTr, yTr, XTe, yTe] = split(y_train, X_train, 0.8, 42);
+[XTr, yTr, XTe, yTe] = split(y_train, X_train, 0.9, 42);
 % Normalize data
 [XTr, XTr_mean, XTr_std] = normalize(XTr);
 XTe = adjust(XTe, XTr_mean, XTr_std);
@@ -38,7 +42,7 @@ errorTeSub = zeros(K, 1);
 errorTrSub = zeros(K, 1);
 for k = 1:K
     [yTrTe, yTrTr, tXTrTe, tXTrTr] = split4crossValidation(k, idxCV, yTr, tXTr);
-    beta = leastSquaresGD(yTrTr, tXTrTr, 0.01);
+    beta = leastSquaresGD(yTrTr, tXTrTr, 0.6);
     errorTeSub(k) = computeCost(yTrTe,tXTrTe,beta); 
     errorTrSub(k) = computeCost(yTrTr,tXTrTr,beta); 
 end
@@ -60,22 +64,25 @@ rmseTe = sqrt(2*mean(errorTeSub))
 %{
 %% Separate data into three different models
 % Seperate data into 3 clouds
+id1 = 1;
+id2 = 2;
+
 tXTr1 = tXTr(yTr<=5500,:);
 yTr1 = yTr(yTr<=5500,:);
-tXTr2 = tXTr(yTr>5500 & yTr<=10000 & tXTr(:,58)>0.25 & tXTr(:,58)<6.4,:);
-yTr2 = yTr(yTr>5500 & yTr<=10000 & tXTr(:,58)>0.25 & tXTr(:,58)<6.4,:);
+tXTr2 = tXTr(yTr>5500 & yTr<=10000 & tXTr(:,id2)>0.25 & tXTr(:,id2)<6.4,:);
+yTr2 = yTr(yTr>5500 & yTr<=10000 & tXTr(:,id2)>0.25 & tXTr(:,id2)<6.4,:);
 tXTr3 = tXTr(yTr>=10000,:);
 yTr3 = yTr(yTr>=10000,:);
 figure;
-plot(tXTr1(:,58),yTr1,'o',tXTr2(:,58),yTr2,'o',tXTr3(:,58),yTr3,'o');
+plot(tXTr1(:,id2),yTr1,'o',tXTr2(:,id2),yTr2,'o',tXTr3(:,id2),yTr3,'o');
 title('Scatter plot of tX\_train vs y\_train');
-hx = xlabel('tX\_train (column 58)');
+hx = xlabel('tX\_train (column id2)');
 hy = ylabel('y\_train');
 set(gca,'fontsize',13,'fontname','Helvetica','box','off','tickdir','out','ticklength',[.02 .02],'xcolor',0.5*[1 1 1],'ycolor',0.5*[1 1 1]);
 set([hx; hy],'fontsize',13,'fontname','avantgarde','color',[.3 .3 .3]);
 print -dpdf cluster.pdf
 figure;
-plot(tXTr1(:,43),yTr1,'o',tXTr2(:,43),yTr2,'o',tXTr3(:,43),yTr3,'o');
+plot(tXTr1(:,id1),yTr1,'o',tXTr2(:,id1),yTr2,'o',tXTr3(:,id1),yTr3,'o');
 
 %% Least squares using normal equations
 disp('Least squares using normal equations 3 models');
@@ -92,15 +99,15 @@ for i = 1:length(th1)
         % Separate into 3 models
         idxTr1 = find(yTrTr<=5500);
         idxTe1 = find(yTrTe<=5500);
-        idxTr2 = find(yTrTr>5500 & yTrTr<=10000 & tXTrTr(:,58)>0.25 & tXTrTr(:,58)<6.4);
-        idxTe2 = find(yTrTe>5500 & yTrTe<=10000 & tXTrTe(:,58)>0.25 & tXTrTe(:,58)<6.4);
+        idxTr2 = find(yTrTr>5500 & yTrTr<=10000 & tXTrTr(:,id2)>0.25 & tXTrTr(:,id2)<6.4);
+        idxTe2 = find(yTrTe>5500 & yTrTe<=10000 & tXTrTe(:,id2)>0.25 & tXTrTe(:,id2)<6.4);
         idxTr3 = find(yTrTr>=10000);
         idxTe3 = find(yTrTe>=10000);
         % Display
 %         figure;
-%         plot(tXTrTr(idxTr1,58),yTrTr(idxTr1,:),'o',tXTrTr(idxTr2,58),yTrTr(idxTr2,:),'o',tXTrTr(idxTr3,58),yTrTr(idxTr3,:),'o');
-%         figure;
-%         plot(tXTrTe(idxTe1,58),yTrTe(idxTe1,:),'o',tXTrTe(idxTe2,58),yTrTe(idxTe2,:),'o',tXTrTe(idxTe3,58),yTrTe(idxTe3,:),'o');
+        plot(tXTrTr(idxTr1,id2),yTrTr(idxTr1,:),'o',tXTrTr(idxTr2,id2),yTrTr(idxTr2,:),'o',tXTrTr(idxTr3,id2),yTrTr(idxTr3,:),'o');
+        figure;
+        plot(tXTrTe(idxTe1,id2),yTrTe(idxTe1,:),'o',tXTrTe(idxTe2,id2),yTrTe(idxTe2,:),'o',tXTrTe(idxTe3,id2),yTrTe(idxTe3,:),'o');
 
         % Compute one beta per model
         beta1 = leastSquares(yTrTr(idxTr1,:), tXTrTr(idxTr1,:));
@@ -110,7 +117,7 @@ for i = 1:length(th1)
         errorTrSub(k) = computeCost3Clouds(yTrTr, tXTrTr, idxTr1, idxTr2, idxTr3, beta1, beta2, beta3);
         errorTeSub(k) = computeCost3Clouds(yTrTe, tXTrTe, idxTe1, idxTe2, idxTe3, beta1, beta2, beta3);
 
-        ypred= doPrediction(tXTrTe, beta1, beta2, beta3, th1(i), th2(j));
+        ypred= doPrediction(tXTrTe, beta1, beta2, beta3, th1(i), th2(j), id1, id2);
         e = yTrTe - ypred;
         errorTeXSelect(k) = e'*e/(2*length(yTrTe));
     end
@@ -124,12 +131,12 @@ rmseT = min(min(err))
 %% Ridge regression using normal equations
 disp('Ridge regression using normal equations');
 degree = 3;
-lvals = logspace(2,2.5,1000);
+lvals = logspace(1,5,100);
 pXTr = [ones(size(XTr, 1), 1) myPoly(XTr, degree)];
 for l = 1:length(lvals)
     lambda = lvals(l);
     %if mod(l,10)==0;
-      %disp('tic');
+    disp('tic');
     %end
     errorTeSub = zeros(K,1);
     errorTrSub = zeros(K,1);
