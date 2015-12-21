52a8887c0a87444f9ee37c149b424ec524f1f732
Necto <necto.ne@gmail.com>
2015-12-21 12:43:50 +0100

    final changes to the report and makefile and predict.m comments

--- prj2/predict.m
+++ prj2/predict.m
@@ -1,6 +1,11 @@
+% Generate the multiclass and binary predicitons for the test data, and
+% save it to the pred_multiclass.mat and pred_binary.mat as required by the
+% project description.
+
 load test/test.mat;
 
-fprintf('Performing multiclass prediction with the ensemble predictor.\n')
+%% Multiclas prediction
+fprintf('Performing multiclass prediction with the ensemble predictor.\n');
 [Ytest, conf] = EnsemblePredict(test);
 
 distr = histc(Ytest, [1 2 3 4]);
@@ -10,6 +15,7 @@ fprintf('Classifier found: %d Airplanes(1), %d Cars(2), %d Horses(3), %d Other(4
 fprintf('Saving the predicted vector.\n');
 save('pred_multiclass.mat', 'Ytest');
 
+%% Binary prediction (based on th multiclass)
 fprintf('Projecting the multiclass prediction to the binary space.\n');
 Ytest = (Ytest ~= 4);
 fprintf('Saving the resulting binary prediction.\n');
